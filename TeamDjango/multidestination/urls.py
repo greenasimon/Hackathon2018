e57d48ddadbfa85f7effddeb9eb473c51c06467b
Greena Simon <sigreena@amazon.com>
2018-10-09 18:08:45 -0400

    changes backend

--- TeamDjango/multidestination/urls.py
+++ TeamDjango/multidestination/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
+    path('search/<str:dest>/', views.results, name='results'),
 
  ]
--- TeamDjango/multidestination/views.py
+++ TeamDjango/multidestination/views.py
@@ -1,11 +1,37 @@
+import requests
+# Create your views here.
+import http.client
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
 def index(request):
     return render(request,'home.html')
 
-def home(request):
-    return render(request,'home.html')
-    
+
+def results(request,dest):
+    api = findBookingAPI(request.path.split('/')[2])
+    qryParams = {"language":"en_US","text":"\"+dest+\""}
+
+
+    headers = {
+        'authorization': "Basic d29tZW5pbnRlY2gyMDE4OjJEY2RDWm5UZmo=",
+        'cache-control': "no-cache",
+        'postman-token': "25ba4d68-b138-6a20-605d-c44b22c976a5"
+    }
+
+    response = requests.request("GET", api, headers=headers, params=qryParams)
+
+    #return HttpResponse(response)
+    return HttpResponse(response.json()["result"][0]['id'])
+
+def findBookingAPI(path):
+    APIDict = {
+        'search':"https://distribution-xml.booking.com/2.2/json/autocomplete"
+    }
+    return APIDict[path]
+
+def getResponse(url):
+    response = requests.get(url)
+    return response
+
